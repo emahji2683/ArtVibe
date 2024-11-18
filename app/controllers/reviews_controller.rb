930c3c51352e212c36d08ae4f89bccc3a652b8bd
emahji2683 <qurrrr31555@gmail.com>
2024-11-18 17:57:57 +0900

    googleplaceAPI関連のコードを一旦削除

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,13 +4,6 @@ class ReviewsController < ApplicationController
   # GET /reviews or /reviews.json
   def index
     @reviews = Review.all
-    @client = GooglePlaces::Client.new(ENV['GOOGLE_PLACES_API_KEY'])
-    @clients = @client.spots(
-      35.681236, 139.767125, # 緯度・経度 (東京駅の例)
-      radius: 1000,          # 半径 1000m
-      :language => 'ja',
-      keyword: '美術館'# 検索キーワード
-    )
   end
 
   # GET /reviews/1 or /reviews/1.json
