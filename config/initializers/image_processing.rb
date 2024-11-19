29dd2a2022f6eccbd8c88df8d7f4894d66c17927
emahji2683 <qurrrr31555@gmail.com>
2024-11-19 21:36:01 +0900

    画像投稿機能の追加

---
+++ config/initializers/image_processing.rb
@@ -0,0 +1,4 @@
+
+require "image_processing/mini_magick"
+
+ImageProcessing::MiniMagick.processor = :mini_magick
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_13_110939) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_18_100627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_110939) do
     t.string "exhibition"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
