bf996d5d0ac648ab08425b2f0f509cacb3a34491
emahji2683 <qurrrr31555@gmail.com>
2024-11-19 10:37:24 +0900

    アソシエーションの作成

---
+++ db/migrate/20241118100627_add_column_reviews.rb
@@ -0,0 +1,5 @@
+class AddColumnReviews < ActiveRecord::Migration[7.2]
+  def change
+   add_column :reviews, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_13_081736) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_18_100627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_081736) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "museums", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -52,6 +59,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_081736) do
     t.string "exhibition"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
