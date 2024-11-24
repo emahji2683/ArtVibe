9c861c3ad92b4288344311e94015462367f57b91
emahji2683 <qurrrr31555@gmail.com>
2024-11-24 18:17:19 +0900

    美術館データ・カラムの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+
+require "csv"
+
+CSV.foreach('db/seeds/csv/museum_each_page_edited.csv', headers: true) do |row|
+  Museum.create(
+    name: row['name'],
+    location: row['location'],
+    business_hours: row['business_hours'],
+    admission_fees: row['admission_fees'],
+    website_url: row['website_url'],
+    phone_number: row['phone_number']
+  )
+end
