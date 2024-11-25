bdb7a0e248b72d7edc39414ab51c052829d63b1d
emahji2683 <qurrrr31555@gmail.com>
2024-11-25 18:21:38 +0900

    museumモデルとreviewモデルのリレーションの追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,11 +8,13 @@ class ReviewsController < ApplicationController
 
   # GET /reviews/1 or /reviews/1.json
   def show
+    @museum = Museum.find(params[:museum_id]) 
   end
 
   # GET /reviews/new
   def new
     @review = Review.new
+    @museum = Museum.find(params[:museum_id]) 
   end
 
   # GET /reviews/1/edit
@@ -22,7 +24,9 @@ class ReviewsController < ApplicationController
   # POST /reviews or /reviews.json
   def create
     @review = current_user.reviews.new(review_params)
-
+    @museum = Museum.find(params[:museum_id]) 
+    @review.museum_id = @museum.id
+    @review.save
     respond_to do |format|
       if @review.save
         format.html { redirect_to @review, notice: "Review was successfully created." }
