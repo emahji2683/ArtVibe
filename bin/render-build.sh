3fc857c67068af875b4a9bd044bcc3137aa2cf40
emahji2683 <qurrrr31555@gmail.com>
2024-11-30 20:22:42 +0900

    本番環境のデータベース修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,5 @@ bundle install
 yarn install
 yarn build # jsファイルをesbuildでバンドルしているため
 bundle exec rake assets:precompile # cssはsprocketsを使っているため
-# bundle exec rake db:migrate # cssはsprocketsを使っているため
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:migrate # cssはsprocketsを使っているため
 
