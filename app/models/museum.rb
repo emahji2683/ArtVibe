dbb0a99d7e29a07ec669a17f822d489d5c2eaa7c
emahji2683 <qurrrr31555@gmail.com>
2024-12-01 20:39:21 +0900

    ransackの実装

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,3 +1,9 @@
 class Museum < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  def self.ransackable_attributes(auth_object = nil)
+    ["admission_fees", "business_hours", "created_at", "id", "location", "name", "phone_number", "updated_at", "website_url"]
+  end
+  def self.ransackable_associations(auth_object = nil)
+    ["reviews"]
+  end
 end
