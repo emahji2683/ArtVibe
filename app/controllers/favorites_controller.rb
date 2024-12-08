aa65c81f9a6a369b00660809dc404ea0e531e8c6
emahji2683 <qurrrr31555@gmail.com>
2024-12-08 17:50:48 +0900

    いいねボタンのajax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,14 +5,28 @@ class FavoritesController < ApplicationController
     # このmuseumのidでfavoriteを作成する
     favorite = current_user.favorites.new(museum_id: museum.id)
     # 保存する
-    favorite.save
-    redirect_to museums_path
+    respond_to do |format|
+      if favorite.save
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update("museum_favorite_-#{museum.id}", partial: 'favorites/btn', locals: { museum: museum })
+        end
+      else
+        format.html { redirect_to museums, alert: 'Failed to favorite.' }
+      end
+    end
   end
 
   def destroy
     museum = Museum.find(params[:museum_id])
     favorite = current_user.favorites.find_by(museum_id: museum.id)
-    favorite.destroy
-    redirect_to museums_path
+    respond_to do |format|
+      if favorite.destroy
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update("museum_favorite_-#{museum.id}", partial: 'favorites/btn', locals: { museum: museum })
+        end
+      else
+        format.html { redirect_to museums, alert: 'Failed to favorite.' }
+      end
+    end
   end
 end
