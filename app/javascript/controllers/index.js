ccd932dff4a16079052f078ca5acfb714eecf054
emahji2683 <qurrrr31555@gmail.com>
2024-12-27 23:19:36 +0900

    Dockerfile等の修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,5 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import LoadingIndicatorController from "./loading_indicator_controller" // 修正箇所
-application.register("loading-indicator", LoadingIndicatorController) // 変更した名前で登録
+import LoadingIndicatorController from "./loading_indicator_controller"
+application.register("loading-indicator", LoadingIndicatorController)
