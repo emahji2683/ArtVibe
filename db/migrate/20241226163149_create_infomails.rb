e6c9c7466050855dd11a1deecdedc8e3508a816d
emahji2683 <qurrrr31555@gmail.com>
2024-12-27 21:52:18 +0900

    migrationの修正V0e

--- db/migrate/20241226163149_create_infomails.rb
+++ db/migrate/20241226163149_create_infomails.rb
@@ -2,7 +2,7 @@ class CreateInfomails < ActiveRecord::Migration[7.2]
   def change
     create_table :infomails do |t|
       t.timestamps
+      t.name :string 
     end
-    remove_column :infomails, :name, :string
   end
 end
